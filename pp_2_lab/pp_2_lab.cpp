94b6f2c6cd9ef057141f3d7f65f39db2441d786c
iaroslav_shavrov <iaroslav_shavrov@practice.relex.ru>
2024-11-29 04:45:02 +0300

    Добавьте файлы проекта.

---
+++ pp_2_lab/pp_2_lab.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+#include <thread>
+#include <vector>
+#include <queue>
+#include <functional>
+#include <mutex>
+#include <condition_variable>
+#include <atomic>
+
+class ThreadPool {
+public:
+    ThreadPool(size_t numThreads);
+    ~ThreadPool();
+
+    template<class F>
+    void enqueue(F&& f);
+
+private:
+    std::vector<std::thread> workers;
+    std::queue<std::function<void()>> tasks;
+
+    std::mutex queueMutex;
+    std::condition_variable condition;
+    std::atomic<bool> stop{ false };
+};
+
+ThreadPool::ThreadPool(size_t numThreads) {
+    for (size_t i = 0; i < numThreads; ++i) {
+        workers.emplace_back([this] {
+            while (true) {
+                std::function<void()> task;
+
+                {
+                    std::unique_lock<std::mutex> lock(this->queueMutex);
+                    this->condition.wait(lock, [this] {
+                        return this->stop || !this->tasks.empty();
+                        });
+                    if (this->stop && this->tasks.empty()) {
+                        return;
+                    }
+                    task = std::move(this->tasks.front());
+                    this->tasks.pop();
+                }
+
+                task();
+            }
+            });
+    }
+}
+
+ThreadPool::~ThreadPool() {
+    {
+        std::unique_lock<std::mutex> lock(queueMutex);
+        stop = true;
+    }
+    condition.notify_all();
+    for (std::thread& worker : workers) {
+        worker.join();
+    }
+}
+
+template<class F>
+void ThreadPool::enqueue(F&& f) {
+    {
+        std::unique_lock<std::mutex> lock(queueMutex);
+        tasks.emplace(std::forward<F>(f));
+    }
+    condition.notify_one();
+}
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    size_t numThreads = std::thread::hardware_concurrency();
+    ThreadPool pool(numThreads);
+
+    for (int i = 0; i < 10; ++i) {
+        pool.enqueue([i] {
+            std::cout << "Задача " << i << " обрабатывается потоком " << std::this_thread::get_id() << std::endl;
+            });
+    }
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    return 0;
+}
